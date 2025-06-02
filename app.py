1650e6ae8dfbd1185f80fc316a4673b9b6a8e2f1
Apoorv Singh <apoorvsingh@Apoorvs-MacBook-Pro.local>
2025-06-02 07:41:13 +0530

    Port update

--- app.py
+++ app.py
@@ -243,4 +243,5 @@ def upcoming_events(uid):
 
 # -------------------- MAIN --------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
